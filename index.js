c5e967f547e55de1e1db1fc1df83c037e580e252
FilEeaZaiR <42099557+FilEeaZaiR@users.noreply.github.com>
2019-01-13 16:33:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -116,6 +116,8 @@ console.log(`un serveur a été enlevé: ${guild.name} (id: ${guild.id}). Il con
     });
 });
 
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 //Début des commandes help :
 client.on(`message`, message =>{
 
@@ -158,4 +160,381 @@ client.on(`message`, message =>{
         }
     });
 }
+});
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//Modération
+client.on(`message`, message =>{
+    
+    let warns = JSON.parse(fs.readFileSync("./warns.json", "utf8"));
+ 
+    if (message.content.startsWith(prefix + "warn")){
+ 
+    if (message.channel.type === "dm") return;
+ 
+    var mentionned = message.mentions.users.first();
+ 
+    if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return message.reply("**:x: Vous n'avez pas la permission `Gérer le serveur` dans ce serveur**").catch(console.error);
+ 
+    if(message.mentions.users.size === 0) {
+ 
+        return message.channel.send("**:x: Vous n'avez mentionnée aucun utilisateur**");
+ 
+    }else{
+ 
+    const args = message.content.split(' ').slice(1);
+ 
+    const mentioned = message.mentions.users.first();
+ 
+    if (message.member.hasPermission('MANAGE_GUILD')){
+ 
+        if (message.mentions.users.size != 0) {
+ 
+            if (args[0] === "<@!"+mentioned.id+">"||args[0] === "<@"+mentioned.id+">") {
+ 
+                if (args.slice(1).length != 0) {
+ 
+                    const date = new Date().toUTCString();
+ 
+                if (warns[message.guild.id] === undefined)
+ 
+                    warns[message.guild.id] = {};
+ 
+                if (warns[message.guild.id][mentioned.id] === undefined)
+ 
+                    warns[message.guild.id][mentioned.id] = {};
+ 
+                const warnumber = Object.keys(warns[message.guild.id][mentioned.id]).length;
+ 
+                if (warns[message.guild.id][mentioned.id][warnumber] === undefined){
+ 
+                    warns[message.guild.id][mentioned.id]["1"] = {"raison": args.slice(1).join(' '), time: date, user: message.author.id};
+ 
+                }else{
+ 
+                    warns[message.guild.id][mentioned.id][warnumber+1] = {"raison": args.slice(1).join(' '),
+ 
+                        time: date,
+ 
+                        user: message.author.id};
+ 
+                }
+ 
+                fs.writeFile("./warns.json", JSON.stringify(warns), (err) => {if (err) console.error(err);});
+ 
+                message.delete();
+ 
+                message.channel.send(':warning: | **'+mentionned.tag+' à été averti**');
+ 
+                message.mentions.users.first().send(`:warning: **Warn |** depuis **${message.guild.name}** donné par **${message.author.username}**\n\n**Raison:** ` + args.slice(1).join(' '))
+ 
+          }else{
+ 
+                message.channel.send("Erreur mauvais usage: "+prefix+"warn <user> <raison>");
+ 
+                }
+ 
+            }else{
+ 
+                message.channel.send("Erreur mauvais usage: "+prefix+"warn <user> <raison>");
+ 
+              }
+ 
+        }else{
+ 
+            message.channel.send("Erreur mauvais usage: "+prefix+"warn <user> <raison>");
+ 
+        }
+ 
+    }else{
+ 
+        message.channel.send("**:x: Vous n'avez pas la permission `Gérer le serveur` dans ce serveur**");
+ 
+    }
+ 
+  }
+ 
+}
+ 
+ 
+ 
+    if (message.content.startsWith(prefix+"seewarns")||message.content===prefix+"seewarns") {
+ 
+        if (message.channel.type === "dm") return;
+ 
+        if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return message.reply("**:x: Vous n'avez pas la permission `Gérer le serveur` dans ce serveur**").catch(console.error);
+ 
+        const mentioned = message.mentions.users.first();
+ 
+        const args = message.content.split(' ').slice(1);
+ 
+        if (message.member.hasPermission('MANAGE_GUILD')){
+ 
+            if (message.mentions.users.size !== 0) {
+ 
+                if (args[0] === "<@!"+mentioned.id+">"||args[0] === "<@"+mentioned.id+">") {
+ 
+                    try{
+ 
+                        if (warns[message.guild.id][mentioned.id] === undefined||Object.keys(warns[message.guild.id][mentioned.id]).length === 0) {
+ 
+                        message.channel.send("**"+mentioned.tag+"** n'a aucun warn :eyes:");
+ 
+                        return;
+ 
+                    }
+ 
+                }catch (err){
+ 
+                    message.channel.send("**"+mentioned.tag+"** n'a aucun warn :eyes:");
+ 
+                    return;
+ 
+                }
+ 
+                    let arr = [];
+ 
+                    arr.push(`**${mentioned.tag}** a **`+Object.keys(warns[message.guild.id][mentioned.id]).length+"** warns :eyes:");
+ 
+                    for (var warn in warns[message.guild.id][mentioned.id]) {
+ 
+                        arr.push(`**${warn}** - **"`+warns[message.guild.id][mentioned.id][warn].raison+
+ 
+                        "**\" warn donné par **"+message.guild.members.find("id", warns[message.guild.id][mentioned.id][warn].user).user.tag+"** a/le **"+warns[message.guild.id][mentioned.id][warn].time+"**");
+ 
+                    }
+ 
+                    message.channel.send(arr.join('\n'));
+ 
+                }else{
+ 
+                    message.channel.send("Erreur mauvais usage: "+prefix+"seewarns <user> <raison>");
+ 
+                    console.log(args);
+ 
+                }
+ 
+            }else{
+ 
+                message.channel.send("Erreur mauvais usage: "+prefix+"seewarns <user> <raison>");
+ 
+            }
+ 
+        }else{
+ 
+            message.channel.send("**:x: Vous n'avez pas la permission `Gérer le serveur` dans ce serveur**");
+ 
+        }
+ 
+    }
+ 
+ 
+ 
+ 
+ 
+    if (message.content.startsWith(prefix+"deletewarns")||message.content===prefix+"deletewarns") {
+ 
+        if (message.channel.type === "dm") return;
+ 
+        if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return message.reply("**:x: Vous n'avez pas la permission `Gérer le serveur` dans ce serveur**").catch(console.error);
+ 
+            const mentioned = message.mentions.users.first();
+ 
+            const args = message.content.split(' ').slice(1);
+ 
+            const arg2 = Number(args[1]);
+ 
+            if (message.member.hasPermission('MANAGE_GUILD')){
+ 
+                if (message.mentions.users.size != 0) {
+ 
+                    if (args[0] === "<@!"+mentioned.id+">"||args[0] === "<@"+mentioned.id+">"){
+ 
+                        if (!isNaN(arg2)) {
+ 
+                            if (warns[message.guild.id][mentioned.id] === undefined) {
+ 
+                                message.channel.send(mentioned.tag+" n'a aucun warn");
+ 
+                                return;
+ 
+                            }if (warns[message.guild.id][mentioned.id][arg2] === undefined) {
+ 
+                                message.channel.send("**:x: Ce warn n'existe pas**");
+ 
+                                return;
+ 
+                            }
+ 
+                            delete warns[message.guild.id][mentioned.id][arg2];
+ 
+                            var i = 1;
+ 
+                                Object.keys(warns[message.guild.id][mentioned.id]).forEach(function(key){
+ 
+                                    var val=warns[message.guild.id][mentioned.id][key];
+ 
+                                    delete warns[message.guild.id][mentioned.id][key];
+ 
+                                    key = i;
+ 
+                                    warns[message.guild.id][mentioned.id][key]=val;
+ 
+                                    i++;
+ 
+                                    });
+ 
+                                fs.writeFile("./warns.json", JSON.stringify(warns), (err) => {if (err) console.error(err);});
+ 
+                                if (Object.keys(warns[message.guild.id][mentioned.id]).length === 0) {
+ 
+                                    delete warns[message.guild.id][mentioned.id];
+ 
+                                }
+ 
+                                message.channel.send(`Le warn de **${mentioned.tag}**\': **${args[1]}** a été enlevé avec succès!`);
+ 
+                                return;
+ 
+                                }if (args[1] === "tout") {
+ 
+                                delete warns[message.guild.id][mentioned.id];
+ 
+                                fs.writeFile("./warns.json", JSON.stringify(warns), (err) => {if (err) console.error(err);});
+ 
+                                message.channel.send(`Les warns de **${mentioned.tag}** a été enlevé avec succès!`);
+ 
+                                return;
+ 
+                            }else{
+ 
+                                message.channel.send("Erreur mauvais usage: "+prefix+"deletewarns <utilisateur> <nombre>");
+ 
+                            }
+ 
+                        }else{
+ 
+                            message.channel.send("Erreur mauvais usage: "+prefix+"deletewarns <utilisateur> <nombre>");
+ 
+                        }
+ 
+                    }else{
+ 
+                        message.channel.send("Erreur mauvais usage: "+prefix+"deletewarns <utilisateur> <nombre>");
+ 
+                    }
+ 
+                }else{
+ 
+                    message.channel.send("**:x: Vous n'avez pas la permission `Gérer le serveur` dans ce serveur**");
+ 
+                }
+ 
+    }
+    
+    if(message.content.startsWith(prefix + "kick")) {
+        
+        if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.channel.send("`Tu ne peux pas faire cette commande !`");
+    
+        if(message.mentions.users.size === 0) {
+            return message.channel.send(`Je veux bien ${message.author.username} mais vous devez mentionner un utilisateur`)
+        }
+
+        var kick = message.guild.member(message.mentions.users.first());
+        if(!kick) {
+            return message.channel.send("Je ne sais si l'utilisateur existe !")
+        }
+
+        if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) {
+            return message.channel.send("Je n'ai pas la perm pour cette commande");
+        }
+
+        kick.kick().then(member => {
+            message.channel.send(`${member.user.username} a été kick par ${message.author.username}`);
+        });
+    }
+
+    if (message.content.startsWith(prefix + "ban")) {
+        if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.channel.send("`Tu ne peux pas faire cette commande !`");
+    
+        if(message.mentions.users.size === 0) {
+            return message.channel.send(`Je veux bien ${message.author.username} mais vous devez mentionner un utilisateur`)
+        }
+
+        var ban = message.guild.member(message.mentions.users.first());
+        if(!ban) {
+            return message.channel.send("Je ne sais si l'utilisateur existe !")
+        }
+
+        if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) {
+            return message.channel.send("Je n'ai pas la perm pour cette commande");
+        }
+
+        ban.ban().then(member => {
+            message.channel.send(`${member.user.username} a été banni par ${message.author.username}`);
+        });
+    }
+
+    if(message.content.startsWith(prefix + "purge") || message.content.startsWith(prefix + "clear")) {
+        let myrole = message.guild.member(client.user).hasPermission("MANAGE_MESSAGES");
+        let yourole = message.guild.member(message.author).hasPermission("MANAGE_MESSAGES");
+    
+        if (!myrole) {
+            return message.channel.send(":no_entry:**Je n'ai pas les permissions nécessaires pour effacer un/des message(s)**");
+        }
+    
+        if (!yourole) {
+            return message.channel.send(":no_entry:**Vous n'avez pas les permissions nécessaires**");
+        }
+    
+        var suppression = message.content.substr(8);
+        if (suppression < 2 || suppression > 10001) {
+            return message.reply(":warning:**La valeur que vous avez entré est invalide, merci de choisir une valeur comprise entre 2 et 10000**");
+        }
+        message.channel.bulkDelete(suppression, true).then(ok => {
+            message.reply("**Suppression de " + "" + suppression + "" + " messages**")
+            .then(message => setTimeout(function(){message.delete()}, 1000))
+            .catch(err => console.log(err));
+        
+        })
+
+    }
+
+    if(message.content.startsWith(prefix + "mute")) {
+        if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas la permission !");
+
+        if(message.mentions.users.size === 0) {
+            return message.channel.send("Tu dois mentionner quelqu'un pour faire cette commande");
+        }
+
+        var mute = message.guild.member(message.mentions.users.first());
+        if(!mute) {
+            return message.channel.send("Je n'ai pas trouvé l'utilisateur ou il l'existe pas !");
+        }
+
+        if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return message.channel.send("Je n'ai pas la permission !");
+        message.channel.overwritePermissions(mute, { SEND_MESSAGES: false}).then(member => {
+            message.channel.send(`${mute.user.username} a été mute par ${message.author.username} !`);
+        })
+    }
+
+    if(message.content.startsWith(prefix + "unmute")) {
+        if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas la permission !");
+
+        if(message.mentions.users.size === 0) {
+            return message.channel.send("Tu dois mentionner quelqu'un pour faire cette commande");
+        }
+
+        var mute = message.guild.member(message.mentions.users.first());
+        if(!mute) {
+            return message.channel.send("Je n'ai pas trouvé l'utilisateur ou il l'existe pas !");
+        }
+
+        if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return message.channel.send("Je n'ai pas la permission !");
+        message.channel.overwritePermissions(mute, { SEND_MESSAGES: true}).then(member => {
+            message.channel.send(`${mute.user.username} a été unmute par ${message.author.username} !`);
+        })
+    }
+    
 });
